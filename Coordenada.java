021731b87e9d78b2c18333d081e2010da7a8745f
XiomaraAmbar <xiomara.ortiz@estudiante.ucsm.edu.pe>
2025-03-19 10:53:59 -0500

    Se añadio metodo de instancia y de clase de la distancia euclideana, y se añadio los métodos toString en Coordenada y Rectangulo

--- Coordenada.java
+++ Coordenada.java
@@ -43,4 +43,25 @@ public class Coordenada {
         return y;
     }
 
+    //metodo de instancia que calcula la distancia euclideana
+    public double distancia(Coordenada c){
+        return Math.sqrt((this.x - c.x) * (this.x - c.x) + (this.y - c.y) * (this.y - c.y));
+    }
+    /* También podria usarse
+    Math.sqrt(Math.pow(this.x - c.x,2) + Math.pow(this.y - c.y,2));
+    seria mejor usarlo si el exponente fuera variable,
+    pero como es el caso de la distancia euclideana el exponente siempre sera 2
+     */
+
+    //metodo de clase que calcula la distancia euclideana
+    public static double distancia(Coordenada c1, Coordenada c2){
+        return Math.sqrt((c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y));
+        //Math.sqrt(Math.pow(c1.x - c2.x,2) + Math.pow(c1.y - c2.y, 2));
+    }
+
+    //metodo que devuelve los valores de una coordenada
+    public String toString(){
+        return "(X: " + this.x + ", Y: " + this.y + ")";
+        //return "Coordenada (x: " + this.x + ", Y: " + this.y + ")";
+    }
 }
--- Rectangulo.java
+++ Rectangulo.java
@@ -3,16 +3,20 @@ public class Rectangulo {
     private Coordenada esquina2;
 
     // Constructor
+    //Define una esquina sin usar setters
+    //Crea una copia de la coordenada
     public Rectangulo(Coordenada c1) {
         this.esquina1 = new Coordenada(c1);
     }
 
     // Constructor
+    //Define dos esquinas usando setters
     public Rectangulo(Coordenada c1, Coordenada c2) {
         setEsquina1(c1);
         setEsquina2(c2);
     }
 
+    //métodos setter
     public void setEsquina1(Coordenada c1) {
         this.esquina1 = c1;
     }
@@ -21,6 +25,7 @@ public class Rectangulo {
         this.esquina2 = c2;
     }
 
+    //métodos getter
     public Coordenada getEsquina1() {
         return esquina1;
     }
@@ -29,4 +34,8 @@ public class Rectangulo {
         return esquina2;
     }
 
+    public String toString() {
+        return "(Esquina1: " + this.esquina1 + ", Esquina2: " + this.esquina2 + ")";
+        //return "Rectangulo (Esquina1: " + this.esquina1 + ", Esquina2: " + this.esquina2 + ")";
+    }
 }
