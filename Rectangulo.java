83bfb59d17e78290e7dc6085d491eca68ad6d9f6
XiomaraAmbar <xiomara.ortiz@estudiante.ucsm.edu.pe>
2025-03-20 10:17:21 -0500

    Añadi un metodo en clase Rectangulo que completa las esquinas 1 y 2 para compararlas en clase verificador

--- Rectangulo.java
+++ Rectangulo.java
@@ -1,6 +1,8 @@
 public class Rectangulo {
     private Coordenada esquina1;
     private Coordenada esquina2;
+    //private Coordenada esquina3;
+    //private Coordenada esquina4;
 
     // Constructor
     //Define una esquina sin usar setters
@@ -10,11 +12,35 @@ public class Rectangulo {
     }
 
     // Constructor
-    //Define dos esquinas usando setters
+    //Define dos esquinas usando setters y se completan las esquinas faltantes
     public Rectangulo(Coordenada c1, Coordenada c2) {
         setEsquina1(c1);
         setEsquina2(c2);
+        //completarEsquinas();
     }
+
+    //metodo define 'x' y 'y'
+    public double definiendoEjes
+
+    /*
+    /*
+    metodo que completa las esquinas del rectangulo
+    esquina3 y esquina4 a partir de esquina1 y esquina2
+     /
+    private void completarEsquinas(){
+        //(x1,y1) y (x2,y2) -> (x2,y1) y (x1,y2)
+        double x1 = esquina1.getX(); //x1
+        double y1 = esquina1.getY(); //y1
+        double x2 = esquina2.getX(); //x2
+        double y2 = esquina2.getY(); //y2
+
+        //Se completan las esquinas 3 y 4 segun las esquinas 1 y 2
+        //(x1,y2)
+        this.esquina3 = new Coordenada(x1,y2);
+        //(x2,y1)
+        this.esquina4 = new Coordenada(x2,y1);
+    }
+    */
 
     //métodos setter
     public void setEsquina1(Coordenada c1) {
@@ -33,6 +59,18 @@ public class Rectangulo {
     public Coordenada getEsquina2() {
         return esquina2;
     }
+
+    /*
+    //Getters de las esquinas completadas esquina3 y esquina4
+
+    public Coordenada getEsquina3(){
+        return esquina3;
+    }
+
+    public Coordenada Esquina4(){
+        return esquina4;
+    }
+    */
 
     public String toString() {
         return "(Esquina1: " + this.esquina1 + ", Esquina2: " + this.esquina2 + ")";
