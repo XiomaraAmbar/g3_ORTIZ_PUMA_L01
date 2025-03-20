42eec3b87e79f33ab2a586b9d8d33d884f8e9cca
XiomaraAmbar <xiomara.ortiz@estudiante.ucsm.edu.pe>
2025-03-20 16:43:52 -0500

    Adicion del metodo addRectangulo de la clase ContainerRect

--- PyPooEje1/src/ContainerRect.java
+++ PyPooEje1/src/ContainerRect.java
@@ -6,7 +6,19 @@ public class ContainerRect {
 
     private static int numRec = 0; //Contador de rectangulos
 
-    addRectangulo(){
-
+    public ContainerRect(int n) {
+        this.rectangulos = new Rectangulo[n];
+        this.distancias = new double[n];
+        this.areas = new double[n];
+        this.n = n;
     }
+
+    public void addRectangulo(Rectangulo rect){
+        if (numRec < n) { // Verifica si hay espacio disponible
+            rectangulos[numRec] = rect;
+        }
+        else{System.out.println("Sin capacidad para mas rectangulos");}
+    }
+
+
 }
